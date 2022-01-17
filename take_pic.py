1976d041e7151fbb5bc5425e6fca28df9c8f987a
dvirhamdi <39012920+dvirhamdi@users.noreply.github.com>
2022-01-17 19:32:11 +0200

    Update take_pic.py

--- take_pic.py
+++ take_pic.py
@@ -5,12 +5,12 @@ import PIL.Image
 import numpy as np
 import matplotlib.pyplot as plt
 from proj import Model,Danse
-
+import HD
 
 def take_pic(record = False):
     photo_list = []
     # define a video capture object
-    vid = cv2.VideoCapture(1)
+    vid = cv2.VideoCapture(0)
 
     while True:
 
@@ -40,8 +40,8 @@ def take_pic(record = False):
 
     img = PIL.Image.fromarray(frame)
 
-    img = np.reshape(np.array((img.resize((128,128)).convert('L'))),(128*128,))
-
+    #img = np.reshape(np.array((img.resize((128,128)).convert('L'))),(128*128,))
+    #return  frame,1
     return img,photo_list
 
 def save_photo_list(path,photo_list,name = ''):
@@ -50,37 +50,80 @@ def save_photo_list(path,photo_list,name = ''):
         p = PIL.Image.fromarray(photo)
         p.save(f'{name}{id}.jpg')
 
+
+def load_image(path):
+    img = np.array((PIL.Image.open(path)))
+    img = HD.face_dedection(img)
+
+    return img
+
+
 rec = input('press enter to take a picture!')
+
 
 if rec.lower() == 'rec':
     test,photo_list = take_pic(record = True)
     save_photo_list(path = 'D:\cyber\yb project\databases\photos',
                     photo_list = photo_list, name='Dvir')
+elif rec.lower() == 'vid':
+    test,photo_list = take_pic(record = True)
+    photo_list = np.array(photo_list[::5])
+    test = photo_list
+    t = []
+    for i in test:
+        t.append(HD.face_dedection(i))
+        print(t)
+    test = np.copy(t)
 else:
     test,photo_list = take_pic(record = False)
+    test = np.array(test)
+    test = HD.face_dedection(test)[0]
+    print(test)
+    test = np.reshape(test,(1,128,128))
+    #test = cv2.resize(test, (128,128), interpolation = cv2.INTER_AREA)
+    #test = np.array(test)
+    #test = cv2.cvtColor(test, cv2.COLOR_BGR2GRAY)
+    #test = np.reshape(test,(1,128,128))
+
+#print(test)
 
 
-os.chdir(r'C:\Users\Dvir hamdi\PycharmProjects\cyberHW\yodbet project')
-plt.imshow(np.reshape(test,(128,128)))
-plt.show()
+test = test / 255
 
+print(test)
+#print(test.shape)
+#test = np.reshape(test,(1,128*128))
+print(test.shape)
+#test = load_image(r'D:\cyber\yb project\databases\photos\test\Dvir\Dvir924.jpg')
+test = np.reshape(test,(test.shape[0],128*128))
 model = Model()
 #model.train()
-layer1 = Danse(n=128, inputs=16384,activtion_function = 'tanh')
-layer2 = Danse(n=3, inputs=128,activtion_function = 'soft_max')
+layer1 = Danse(n=16384//2, inputs=16384,activtion_function = 'relu')
+layer2 = Danse(n=256, inputs=16384//2,activtion_function = 'relu')
+layer3 = Danse(n=4, inputs=256,activtion_function = 'soft_max')
 
 model.add(layer1)
 model.add(layer2)
+model.add(layer3)
+
+
 
 model.compile(loss='crossEntropy',lr = 0.00001)
 
-pred_values = {0:'Dvir',1:'Dan',2:'Ron'}
+pred_values = {0:'Dan',1:'Dvir',2:'Fefer',3:'Guy'}
 
-model.load('model.p')
-res = model.predict([test])
+model.load(r'C:\Users\Dvir hamdi\PycharmProjects\cyberHW\yodbet project\model.p')
+
+
+
+
+os.chdir(r'C:\Users\Dvir hamdi\PycharmProjects\cyberHW\yodbet project')
+plt.imshow(np.reshape(test[0],(128,128)))
+plt.show()
+
+res = model.predict(test)
 print('predictions:',res)
-print(pred_values)
-print(pred_values[np.argmax(res[0])])
+print('pred:',np.argmax(res,axis=1))
 
 
 
